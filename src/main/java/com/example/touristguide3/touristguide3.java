24211839e4d585e19c37fa254b37120be17ad2fe
Uller <fullersted@gmail.com>
2025-10-07 11:04:06 +0200

    made ready for deployment

--- src/main/java/com/example/touristguide3/touristguide3.java
+++ src/main/java/com/example/touristguide3/touristguide3.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TouristGuide3 {
+public class touristguide3 {
 
     public static void main(String[] args) {
-        SpringApplication.run(TouristGuide3.class, args);
+        SpringApplication.run(touristguide3.class, args);
     }
 }
